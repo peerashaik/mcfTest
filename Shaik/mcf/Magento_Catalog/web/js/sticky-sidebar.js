eb508b5087f6d1c50eb680866af6b58af7c60471
Shaik <peershaik786@gmail.com>
2024-10-17 10:23:10 +0530

    Gallery changes as expected

--- Shaik/mcf/Magento_Catalog/web/js/sticky-sidebar.js
+++ Shaik/mcf/Magento_Catalog/web/js/sticky-sidebar.js
@@ -2,13 +2,12 @@ require(['jquery', 'domReady!'], function($){
    "use strict";
 
     $(window).on('scroll', function () {
-        let bottomPos = $('.product-images');
         if($(window).scrollTop() >= 100) {
             $('.product.info.detailed').addClass('fixed');
             $('.product-info-main').addClass('fixed');
         }
         if ($(window).scrollTop() >= $(
-            '.product-images').offset().top + $('.product-images').
+            '.product.media').offset().top + $('.product.media').
                 outerHeight() - window.innerHeight) {
             $('.product.info.detailed').removeClass('fixed');
             $('.product-info-main').removeClass('fixed');
